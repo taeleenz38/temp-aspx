01e1dde7e4ef89e4f89ca04eb6ad2607a8c64e25
Tae Lee <104185523+taeleenz38@users.noreply.github.com>
2025-07-15 18:22:11 +1000

    Update Projects.tsx

--- app/components/layout/work/Projects.tsx
+++ app/components/layout/work/Projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { motion as MotionComponent } from "framer-motion";
 import ProjectsCarousel from "../../ui/ProjectsCarousel";
 const projectData = [
