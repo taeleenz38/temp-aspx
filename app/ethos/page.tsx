ea77518843a40bddd085937242b6f2e8e7e04cab
BM-Tae <tae@blockmajority.io>
2025-07-15 12:07:49 +1000

    build fix

--- app/ethos/page.tsx
+++ app/ethos/page.tsx
@@ -3,9 +3,9 @@ import Section1 from "./sections/Section1";
 import Section2 from "./sections/Section2";
 import Section3 from "./sections/Section3";
 import Section4 from "./sections/Section4";
-import Section5 from "./sections/Section5";
-import Section6 from "./sections/Section6";
-import Section7 from "./sections/Section7";
+// import Section5 from "./sections/Section5";
+// import Section6 from "./sections/Section6";
+// import Section7 from "./sections/Section7";
 import Section8 from "./sections/Section8";
 
 const Ethos = () => {
