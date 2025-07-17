fee31d61cd6ebc21ca2464ea495446473c85850d
Tae Kyu Lee <taekyu.lee3@gmail.com>
2025-07-17 16:23:20 +1000

    joseph headshot

--- app/components/data/PeoplesData.tsx
+++ app/components/data/PeoplesData.tsx
@@ -12,38 +12,31 @@ const peopleData = [
     title: "Chief Creative Office & Co-Founder",
   },
   {
-    category: [
-      "leadership",
-      "producers",
-      "creators",
-    ],
+    category: ["leadership", "producers", "creators"],
     headshot: "/headshots/liam.jpg",
     name: "Liam Prince",
     title: "Chief Executive Officer & Co-Founder",
   },
   {
-    category: [
-      "designers",
-      "leadership",
-      "producers",
-      "creators",
-    ],
+    category: ["designers", "leadership", "producers", "creators"],
     headshot: "/headshots/tobias.jpg",
     name: "Tobias Taylor",
     title: "Chief Marketing Officer",
   },
   {
-    category: [
-      "talent",
-    ],
+    category: ["leadership"],
+    headshot: "/headshots/joseph.jpg",
+    name: "Joseph Clark-Carrera",
+    title: "VP of Talent Discovery",
+  },
+  {
+    category: ["talent"],
     headshot: "/headshots/192sara.jpg",
     name: "192sara",
     title: "Designer & Internet Personality",
   },
   {
-    category: [
-      "talent",
-    ],
+    category: ["talent"],
     headshot: "/headshots/corey.jpg",
     name: "Corey Lingo",
     title: "Musician",
